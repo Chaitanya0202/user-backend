8a7f92eea37ecc51c2387a05913da52471fcee4a
Chaitanya0202 <115619207+Chaitanya0202@users.noreply.github.com>
2024-11-21 20:19:11 +0530

    first commit

--- src/main/java/com/user/user_full_stack_project/controller/UserController.java
+++ src/main/java/com/user/user_full_stack_project/controller/UserController.java
@@ -10,15 +10,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.user.user_full_stack_project.dto.User;
 import com.user.user_full_stack_project.service.UserService;
 
 @RestController
-@CrossOrigin("http://localhost:3000/")
+@CrossOrigin("http://localhost:3000")
 //@RequestMapping("/user")
 public class UserController {
 	@Autowired
@@ -48,5 +48,6 @@ public class UserController {
 	public User getUserById(@PathVariable int id) {
 		return service.getUserById(id);
 	}
+	
 
 }
--- src/main/java/com/user/user_full_stack_project/dto/UserRequest.java
+++ src/main/java/com/user/user_full_stack_project/dto/UserRequest.java
@@ -1,28 +1,23 @@
 package com.user.user_full_stack_project.dto;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.Data;
-
-@Entity
 @Data
-public class User {
+public class UserRequest {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
 	private String name;
 	private String email;
 	private long phone;
 	private String password;
 	
-//	@Lob
-//	 private byte[] profileImage;
-	
-	
-	
+	private MultipartFile profileImage;
 
 }
